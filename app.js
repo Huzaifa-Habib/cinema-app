0462ff9cc73d1abda66a99abe974b3cf7d76ffae
Huzaifa Habib <101453901+cocobutte@users.noreply.github.com>
2022-09-20 17:58:01 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,113 @@
+
+var get = window.localStorage.getItem('userData');
+var object = JSON.parse(window.localStorage.getItem('userData'));
+console.log(get)
+for (var key in object) {
+   console.log( object[key])
+}
+
+var userName = document.getElementById("userName")
+userName.value = object[key].userData
+var gmail = document.getElementById("gmail");
+gmail.value = object[key].userGmail
+var address = document.getElementById("address")
+address.value = object[key].userAddress
+var pass = document.getElementById("pass");
+pass.value = object[key].userPass
+var dob = document.getElementById("dob");
+dob.value = object[key].userDOB
+var postal = document.getElementById("postal");
+postal.value = object[key].userCode
+var phone = document.getElementById("phone")
+phone.value = object[key].userNo
+
+var dropD = document.getElementById("drop")
+dropD.style.display = "none"
+
+var dropdown= document.getElementById("drop")
+dropdown.style.display="none"
+
+
+function p () {
+    if (dropdown.style.display=="none"){
+                dropdown.style.display="flex"
+               
+            }
+            else{
+                dropdown.style.display="none"
+        
+        
+            }
+
+}
+
+
+var profile = document.getElementById("pro");
+var modal = document.getElementById("info") 
+
+function modalF() {
+      modal.style.display="block"
+      dropdown.style.display="none"
+
+
+      modal.animate([   
+         // { transform: 'translateY(660px)'},
+         { transform: 'translateY(-660px)'},
+         {transition: '2s'}
+     ],{
+         duration: 1000
+     });
+     
+
+
+}
+
+function closeM(){
+   modal.style.display="none"
+}
+
+let moviesSection = document.getElementById("moviesSection")
+let ticketSec = document.getElementById("ticketMainSec")
+
+
+
+
+
+
+
+
+
+function main() {
+   var k = document.getElementById("j")
+   k.setAttribute("href", "../../../index.html")
+    var object = JSON.parse(window.localStorage.getItem('userData'));
+
+    let getPrevData =JSON.parse(window.localStorage.getItem('userData'));
+     for(var key in getPrevData){
+       if(  getPrevData[key].logInUser  === true){
+           getPrevData[key].logInUser = false
+           console.log(getPrevData[key])
+           localStorage.setItem('userData',JSON.stringify(getPrevData))
+
+           
+       }
+       
+   }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
